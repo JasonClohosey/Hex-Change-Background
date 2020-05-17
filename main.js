94b8e567d7d54683e43ff666fdf8c0a35f0a2570
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-17 15:33:57 -0400

    Finished JavaScript/Fully functional

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert ("change colors");
+document.getElementById("colorBtn").addEventListener("click", function() {
+
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    
+   
+        document.getElementById("color").innerHTML = ("#" + randomColor);
+
+    document.body.style.background = "#" + randomColor;
+});
